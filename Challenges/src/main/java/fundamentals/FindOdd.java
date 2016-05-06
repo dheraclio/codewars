d35847b856db1d7fa2f65f393a07735f944eb87c
Daniel Heráclio <dheraclio@gmail.com>
2016-05-05 22:41:35 -0300

    Added solutions found oncodewars for comparison

--- Challenges/src/main/java/fundamentals/FindOdd.java
+++ Challenges/src/main/java/fundamentals/FindOdd.java
@@ -2,7 +2,10 @@ package fundamentals;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.BiConsumer;
+import java.util.TreeSet;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.function.Function;
 
 /**
  * Given an array, find the int that appears an odd number of times.
@@ -16,12 +19,11 @@ public class FindOdd {
 
   /**
    *
-   * O(n*n) solution
-   * @param A array of int to search
+   * @param array of int to search
    * @return the odd number
    * @throws RuntimeException case od int not found
    */
-  public static int findIt(int[] A) {
+   public static int findIt(int[] A) {
     //Uses a map to count int occurrences
     HashMap<Integer, Integer> map = new HashMap<>(A.length);
     for (int i = 0; i < A.length; i++)
@@ -33,4 +35,44 @@ public class FindOdd {
 
     throw new RuntimeException("Odd int not found");
   }
+
+/*
+  //Best solution found on Codewars to me
+  public static int findIt(int[] A) {
+    int xor = 0;
+    //Since the problem stats that there will always be a single odd int, the XOR works.
+    for (int i : A) {
+      xor ^= i;
+    }
+    return xor;
+  }
+*/
+/*
+//Java 8 solution, slow but ok
+public static int findIt(int[] a) {
+    return Arrays.stream(a)
+                 .boxed()
+                 .collect(Collectors.groupingBy(Function.identity()))
+                 .entrySet()
+                 .stream()
+                 .filter(e -> e.getValue().size() % 2 == 1)
+                 .mapToInt(e -> e.getKey())
+                 .findFirst()
+                 .getAsInt();
+    }
+*/
+/*
+//Clever but too expensive
+  public static int findIt(int[] A) {
+    final TreeSet<Integer> set = new TreeSet<>();
+    for (int x : A) {
+      if (set.contains(x)) {
+        set.remove(x);
+      } else {
+        set.add(x);
+      }
+    }
+    return set.first();
+  }
+  */
 }
